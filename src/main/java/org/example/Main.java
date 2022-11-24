7a39a9d0081d41bc4a8aa4660cbe477f198859cc
308 <308@academicos.cesde.edu.co>
2022-11-24 11:27:40 -0500

    Se termina examen de moviles 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,10 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entradaPorTeclado=new Scanner(System.in);
-        Jugador objetoJugador = new Jugador();
         ArrayList<Jugador> jugadores=new ArrayList<Jugador>();
-
-
 
         int variableDeControl=0;
         System.out.println("-----------------------------------------------------------------");
@@ -29,45 +26,69 @@ public class Main {
             variableDeControl = entradaPorTeclado.nextInt();
             switch (variableDeControl) {
                 case 1:
-                    int convocados;
-                    if(convocados>=1){
+                    Jugador objetoJugador = new Jugador();
 
-                        convocados(convocados-1);
+                    System.out.print("\nIngresa el numero de la camiseta: ");
+                    objetoJugador.setNumeroCamiseta(entradaPorTeclado.nextInt());
+                    entradaPorTeclado.nextLine();
+                    System.out.print("Ingresa el nombre del jugador: ");
+                    objetoJugador.setNombre(entradaPorTeclado.nextLine());
+                    System.out.print("Ingresa los apellidos del jugador: ");
+                    objetoJugador.setApellidos(entradaPorTeclado.nextLine());
+                    System.out.print("Ingresa la posicion en la que juega el jugador \n(Delantero, defensa, mediocampista): ");
+                    objetoJugador.setPosicion(entradaPorTeclado.nextLine());
+                    System.out.print("Ingresa la edad del jugador: ");
+                    objetoJugador.setEdad(entradaPorTeclado.nextInt());
+                    entradaPorTeclado.nextLine();
+                    System.out.print("Ingresa el equipo en donde juega el jugador: ");
+                    objetoJugador.setEquipoDondeJuega(entradaPorTeclado.nextLine());
 
-                        //Si voy a guardar objetos de una clase CUALQUIERA
-                        //en una arraylist primero debo crear un objeto
-                        //de esa clase cualquiera
-                        Jugador objetoJugador = new Jugador();
+                    jugadores.add(objetoJugador);
+                    break;
+                case 2:
+                    System.out.print("Ingresa el numero del jugador que desea buscar: ");
+                    int buscarCamiseta = entradaPorTeclado.nextInt();
+                    for (Jugador jugador: jugadores) {
+                        if (buscarCamiseta==jugador.getNumeroCamiseta()) {
+                            System.out.print("\nEl jugador buscado es: \n" + "Numero Camiseta: " + jugador.getNumeroCamiseta() + "\n" + "Nombre: " + jugador.getNombre() + "\n" +
+                                    "Apellido: " + jugador.getApellidos() + "\n" + "Posicion: " + jugador.getPosicion() + "\n"
+                                    + "Edad: " + jugador.getEdad() + "\n" + "Equipo: " + jugador.getEquipoDondeJuega() + "\n");
 
-                        //LLENAR/PEDIR ALMACENAR/SETEAR/LLEVAR LOS ATRIBUTOS
-                        //DEL OBJETO QUE ACABO DE CREAR
-                        System.out.print("\nDigita el numero de la camiseta: ");
-                        objetoJugador.setNumeroCamiseta(entradaPorTeclado.nextInt());
-                        entradaPorTeclado.nextLine();
-                        System.out.print("\nDigita el nombre del jugador: ");
-                        objetoJugador.setNombre(entradaPorTeclado.nextLine());
-                        System.out.print("\nDigita los apellidos del jugador: ");
-                        objetoJugador.setApellidos(entradaPorTeclado.nextLine());
+                        }else {
+                            System.out.println("El jugador que desea buscar no se encuentra en la convocatoria.");
+                        }
+                    }
+                    break;
+                case 3:
+                    boolean i = false;
+                    System.out.print("\nDigite el numero de camiseta del jugador a editar: ");
+                    int numeroCamiseta = entradaPorTeclado.nextInt();
+                    for (Jugador jugador: jugadores) {
+                        if (numeroCamiseta==jugador.getNumeroCamiseta()) {
+                            i = true;
+                            System.out.print("\nIngresa el nuevo numero de la camiseta: ");
+                            jugador.setNumeroCamiseta(entradaPorTeclado.nextInt());
+                            entradaPorTeclado.nextLine();
+                            System.out.print("Ingresa la posicion a la que desea cambiar \n(Delantero, defensa, mediocampista): ");
+                            jugador.setPosicion(entradaPorTeclado.nextLine());
+                            System.out.print("Ingresa el equipo en donde juega el jugador: ");
+                            jugador.setEquipoDondeJuega(entradaPorTeclado.nextLine());
+                        } else{
+                            i = false;
+                        }
+                    }
 
-                        //Si ya tiene mi objeto entonces agregamos
-                        //el objeto a la lista
-                        jugadores.add(objetoJugador);
-
-                    }else {
-
-                        System.out.println("Aragan, no te entra mas gente ahi\n");
-
+                    if (i==false){
+                        System.out.println("El numero de camiseta que busca no se encuentra en la convocatoria");
                     }
 
                     break;
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
                 case  4:
-                    //23 JUGADORES
+                    for (Jugador jugador:jugadores){
+                        System.out.println("\nN° de la camiseta: "+jugador.getNumeroCamiseta()+"\n"+"Nombre: "+jugador.getNombre()+"\n"+"Apellidos: "+jugador.getApellidos()+"\n"+
+                                "Posicion en la que juega: "+jugador.getPosicion()+"\n"+"Edad: "+jugador.getEdad()+"\n"+
+                                "Equipo al que pertenece: "+jugador.getEquipoDondeJuega()+"\n");
+                    }
                     break;
 
                 default:
